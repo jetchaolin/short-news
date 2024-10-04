5961ffc589174b1a8131e166e8bcbe0ebda2ebb4
Jetulio <joo_ramos2006@hotmail.com>
2024-10-04 16:28:55 -0300

    Added article search feature

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,9 @@
 class Article < ApplicationRecord
   belongs_to :category
   validates :title, :published_at, :body, presence: true
+
+  def self.search(search)
+    where("title LIKE ? OR body LIKE ?", "%#{search}%", "%#{search}%")
+    # where("title LIKE :search OR body LIKE :search OR category LIKE :search", search: "%#{search}%")
+  end
 end
