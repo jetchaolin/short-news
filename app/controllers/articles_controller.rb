00ee10d6687cf4ebf175029f9343fee121677b42
Jetulio <joo_ramos2006@hotmail.com>
2024-10-08 15:53:04 -0300

    Added Admin with exclusive functions and fixed Pagy issue

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: %i[ show update destroy ]
+  before_action :set_article, only: %i[ show edit update destroy ]
+  # before_action :authenticate_admin!
   # before_action :load_articles, only: %i[ index ]
   def index
     # @pagy, @articles = pagy_countless(Article.order(published_at: :desc), limit: 3)
@@ -7,18 +8,22 @@ class ArticlesController < ApplicationController
     # render "scrollable_list" if params[:page]
     if params[:search]
       @search = Article.search(params[:search])
-      @pagy, @articles = pagy_countless(@search.order(published_at: :desc), limit: 3)
+      if @search.count > 3
+        @pagy, @articles = pagy_countless(@search.order(published_at: :desc), limit: 3, last_page: @search.count)
+      end
 
       render "scrollable_list" if params[:page]
     elsif params[:category_id]
         @category = Category.includes(:articles).find(params[:category_id])
         @articles = @category.articles.order(published_at: :desc)
 
-        @pagy, @articles = pagy_countless(@articles, limit: 3)
+        if @category.articles.count > 3
+          @pagy, @articles = pagy_countless(@articles, limit: 3, last_page: @category.articles.count)
+        end
 
         render "scrollable_list" if params[:page]
     else
-      @pagy, @articles = pagy_countless(Article.order(published_at: :desc), limit: 3)
+      @pagy, @articles = pagy_countless(Article.order(published_at: :desc), limit: 3, last_page: (Article.count / 3))
 
       render "scrollable_list" if params[:page]
     end
@@ -38,10 +43,18 @@ class ArticlesController < ApplicationController
     redirect_to root_path
   end
 
+  def edit
+    @article
+  end
+
   def update
+    @article.update(article_params)
+    redirect_to root_path
   end
 
   def destroy
+    @article.destroy!
+    redirect_to root_path
   end
 
   def user
